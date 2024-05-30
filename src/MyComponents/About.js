cdb7886a6b4e746cc8c6dea645cf3c9f884d8bcb
20antara <antara20_ug@cse.nits.ac.in>
2024-05-30 21:11:57 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      About: 
+      <p>This is a todo list generator to keep a track of daily chores.</p>
+    </div>
+  )
+}
+
+//export default About
